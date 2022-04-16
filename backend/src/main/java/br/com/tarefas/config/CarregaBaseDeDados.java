37b8dc0257ec33d986179acd2ef3adf28ca2b4ea
Jordan <jordanrafael92@gmail.com>
2022-04-16 16:19:50 -0300

    Created test using spring test

--- backend/src/main/java/br/com/tarefas/config/CarregaBaseDeDados.java
+++ backend/src/main/java/br/com/tarefas/config/CarregaBaseDeDados.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import br.com.tarefas.model.Tarefa;
 import br.com.tarefas.model.TarefaCategoria;
@@ -16,7 +17,7 @@ import br.com.tarefas.repository.TarefaRepository;
 import br.com.tarefas.repository.UsuarioRepository;
 
 @Configuration
-//@Profile("dev")
+@Profile("dev")
 public class CarregaBaseDeDados {
 	
 	@Autowired
--- backend/src/main/java/br/com/tarefas/services/TarefaService.java
+++ backend/src/main/java/br/com/tarefas/services/TarefaService.java
@@ -7,7 +7,9 @@ import javax.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.tarefas.exception.TarefaStatusExeption;
 import br.com.tarefas.model.Tarefa;
+import br.com.tarefas.model.TarefaStatus;
 import br.com.tarefas.repository.TarefaRepository;
 
 @Service
@@ -34,5 +36,16 @@ public class TarefaService {
 	
 	public void deleteById(Integer id) {
 		repository.deleteById(id);
+	}
+	
+	public Tarefa iniciarTarefaPorId(Integer id) {
+		Tarefa tarefa = getTarefaPorId(id);
+		
+		if (!TarefaStatus.ABERTO.equals(tarefa.getStatus()))
+			throw new TarefaStatusExeption();
+		
+		tarefa.setStatus(TarefaStatus.EM_ANDAMENTO);
+		repository.save(tarefa);
+		return tarefa;
 	}
 }
