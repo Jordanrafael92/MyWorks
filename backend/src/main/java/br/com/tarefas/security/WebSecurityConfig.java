e05f78fc48090d75d504049ef3651270d0e7992d
Jordan <jordanrafael92@gmail.com>
2022-05-03 16:25:52 -0300

    Initial settings - spring security

---
+++ backend/src/main/java/br/com/tarefas/security/WebSecurityConfig.java
@@ -0,0 +1,44 @@
+package br.com.tarefas.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		// TODO Auto-generated method stub
+		return super.authenticationManagerBean();
+	}
+	
+	//Autenticação
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+		super.configure(auth);
+	}
+	
+	//Autorização
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+
+		super.configure(http);
+	}
+}
